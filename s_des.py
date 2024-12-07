cc5bfc8ecfdc5d3a0af2cfac6b45f8e154c1020e
Pedro Farias <pfarias29@gmail.com>
2024-12-07 15:32:44 -0300

    S-DES finalizado

--- s_des.py
+++ s_des.py
@@ -40,7 +40,7 @@ def key_generation(key):
 
     # Seleciona 8 bits e faz uma permutação
     P8 = [6, 3, 7, 4, 8, 5, 10, 9]
-    K1 = [0 for i in range(8)]
+    K1 = [0 for _ in range(8)]
     for i in range(8):
         K1[i] = int(circular_shift[P8[i] - 1])
 
@@ -67,13 +67,59 @@ def f_function(L, R, key):
                              [2, 3, 4, 1]]
     
     # Expansão/Permutação de R e XOR com a chave
+    ## Cria uma matriz 2x4 para armazenar os valores de R e a chave
+    ## Faz um XOR com os valores booleanos de R e a chave e transforma em inteiros
     matrix = [[0] * 4 for _ in range(2)]
     for i in range(2):
         for j in range(4):
-            matrix[i][j] = int(bool(R[expansion_permutation[i][j] - 1]) ^ bool(key[i + j]))
+            matrix[i][j] = int(bool(int(R[expansion_permutation[i][j] - 1])) ^ bool(key[(4 * i) + j]))
 
-    print(matrix)
+    print("Matriz de expansão/permutação: ", matrix, sep="")
 
+    # S-Boxes
+    S0 = [[1, 0, 3, 2],
+          [3, 2, 1, 0],
+          [0, 2, 1, 3],
+          [3, 1, 3, 2]]
+    
+    S1 = [[0, 1, 2, 3],
+          [2, 0, 1, 3],
+          [3, 0, 1, 0],
+          [2, 1, 0, 3]]
+    
+    matrix_S_Box = []
+    # Pega os valores da matriz e transforma em binário
+    ## P(i,0) + P(j,3) = linha
+    ## P(i,1) + P(j,2) = coluna
+    for i in range(2):
+        if i == 0:
+            row = int(str(matrix[i][0]) + str(matrix[i][3]), 2)
+            col = int(str(matrix[i][1]) + str(matrix[i][2]), 2)
+            res = list(format(S0[row][col], '02b'))
+            matrix_S_Box += res
+        else:
+            row = int(str(matrix[i][0]) + str(matrix[i][3]), 2)
+            col = int(str(matrix[i][1]) + str(matrix[i][2]), 2)
+            res = list(format(S1[row][col], '02b'))
+            matrix_S_Box += res
+    
+    # Permutação pós-S-Boxes
+    P4 = [2, 4, 3, 1]
+    final_permutation = [0 for i in range(4)]
+    for i in range(4):
+        final_permutation[i] = matrix_S_Box[P4[i] - 1]
+
+
+    # XOR com L
+    final_res = [0 for i in range(4)]
+    for i in range(4):
+        final_res[i] = str(int(bool(int(L[i])) ^ bool(int(final_permutation[i]))))
+
+
+    # print("Resultado da função F: ", final_res, sep="")
+
+    # Retorna o lado esquerdo e direito, respectivamente
+    return final_res, R
             
 
 
@@ -83,20 +129,31 @@ def s_des(key, block):
 
     # Permutação Inicial
     IP = [2, 6, 3, 1, 4, 8, 5, 7]
-    first_permutation = [0 for i in range(8)]
+    first_permutation = [0 for _ in range(8)]
     for i in range(8):
-        first_permutation[i] = int(block[IP[i] - 1])
+        first_permutation[i] = block[IP[i] - 1]
+
+    first_permutation = "".join(first_permutation)
+
 
     # Divisão do bloco de texto em duas partes
-    L0 = block[:4]
-    R0 = block[4:]
-    f_function(L0, R0, K1)
+    L0 = list(first_permutation[:4])
+    R0 = list(first_permutation[4:])
+    L1, R1 = f_function(L0, R0, K1)
 
-    
+    L2, R2 = f_function(L1, R1, K2)
+
+    print(L2 + R2)   
+    final_block = L2 + R2
+
+    # Permutação Final    
     IP_1 = [4, 1, 3, 5, 7, 2, 8, 6]
+    IP_1_permutation = [0 for i in range(8)]   
     for i in range(8):
-        print(first_permutation[IP_1[i] - 1], end="")
+        IP_1_permutation[i] = final_block[IP_1[i] - 1]
 
+    IP_1_permutation = "".join(IP_1_permutation)
+    print("Bloco de texto encriptado: ", IP_1_permutation, sep="")
     
 
     return 0
